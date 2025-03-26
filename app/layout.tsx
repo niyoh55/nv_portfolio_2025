87e358d22c039f938ef87ab6931980cceffffea0
niyoh55 <niyohedwyn21@gmail.com>
2025-03-27 02:17:57 +0800

    Initial changes for design of portfolio

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,11 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import {
+  Geist,
+  Geist_Mono,
+  Roboto,
+  Noto_Sans,
+  DM_Sans,
+} from "next/font/google";
 import "./globals.css";
 
 const geistSans = Geist({
@@ -10,6 +16,18 @@ const geistSans = Geist({
 const geistMono = Geist_Mono({
   variable: "--font-geist-mono",
   subsets: ["latin"],
+});
+
+const roboto = Roboto({
+  variable: "--font-roboto",
+});
+
+const notoSans = Noto_Sans({
+  variable: "--font-noto-sans",
+});
+
+const dmSans = DM_Sans({
+  variable: "--font-dm-sans",
 });
 
 export const metadata: Metadata = {
@@ -25,7 +43,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${geistSans.variable} ${geistMono.variable} ${roboto.variable} ${notoSans.variable} ${dmSans.variable} antialiased`}
       >
         {children}
       </body>
--- app/page.tsx
+++ app/page.tsx
@@ -1,103 +1,229 @@
-import Image from "next/image";
+"use client";
+
+import gsap from "gsap";
+import Link from "next/link";
+import { useEffect, useRef } from "react";
 
 export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm/6 text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-[family-name:var(--font-geist-mono)] font-semibold">
-              app/page.tsx
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
+  const $follower = useRef<HTMLDivElement>(null);
+  const aboutMeRef = useRef(null);
+  const experienceRef = useRef(null);
+  const projectsRef = useRef(null);
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+  useEffect(() => {
+    const ctx = gsap.context(() => {
+      const xTo = gsap.quickTo($follower.current, "x", {
+        duration: 2,
+        ease: "power3",
+      });
+
+      const yTo = gsap.quickTo($follower.current, "y", {
+        duration: 2,
+        ease: "power3",
+      });
+
+      window.addEventListener("mousemove", (e) => {
+        xTo(e.clientX);
+        yTo(e.clientY);
+      });
+
+      return () =>
+        window.removeEventListener("mousemove", (e) => {
+          xTo(e.clientX);
+          yTo(e.clientY);
+        });
+    });
+
+    return () => {
+      ctx.revert();
+    };
+  }, []);
+
+  const handleScroll = (event) => {
+    event.preventDefault(); // Prevent the default anchor link behavior
+    const targetId = event.target.getAttribute("href").substring(1); // Get the section ID
+
+    const targetElement = document.getElementById(targetId);
+    if (targetId === "about-me//") {
+      window.scrollTo({ top: 0, behavior: "smooth" });
+    } else if (targetElement) {
+      targetElement.scrollIntoView({ behavior: "smooth" }); // Smooth scrolling
+    }
+  };
+
+  return (
+    <div className="font-[family-name:var(--font-dm-sans)] min-h-sceen overflow-y-scroll bg-[#090f3f] text-[#D4ADFC]">
+      <div
+        ref={$follower}
+        className="pointer-events-none fixed left-0 top-0 aspect-square w-[40vmin] -translate-x-1/2 -translate-y-1/2 overflow-hidden rounded-full will-change-transform"
+      >
+        <div className="flex items-center justify-center w-full h-full duration-75 rounded-full bg-radial from-gray-100/10 to-transparent animate-pulse">
+          {/* <div className="h-[80%] w-[80%] bg-gradient-to-tl from-white/0 via-gray-100/100 to-transparent rounded-full justify-center items-center flex">
+            <div className="h-[60%] w-[60%] bg-gradient-to-tl from-white/0 via-gray-100/100 to-transparent rounded-full justify-center items-center flex">
+              <div className="h-[40%] w-[40%] bg-gradient-to-tl from-white/0 via-gray-100/100 to-transparent rounded-full justify-center items-center flex"></div>
+            </div>
+          </div> */}
+        </div>
+      </div>
+      <main className="flex h-screen flex-row gap-[32px] items-center sm:items-start px-32 py-24">
+        {/* Fixed Side */}
+        <div className="fixed w-1/2 h-full px-32">
+          <div className="flex flex-col items-start justify-center gap-1">
+            <h1 className="text-5xl font-extrabold">Niyoh Edwyn Villanueva</h1>
+            <h2 className="text-2xl font-extrabold">Software Engineer</h2>
+            <div className="w-[80%]">
+              <h3 className="text-base font-semibold">
+                I develop highly accessible and pixel perfect applications for
+                mobile and the web.
+              </h3>
+            </div>
+          </div>
+          <div className="flex flex-col items-start justify-start py-20 text-base font-semibold gap-y-2">
+            <Link
+              className="duration-500 hover:text-2xl"
+              href="#about-me"
+              onClick={handleScroll}
+            >
+              About Me
+            </Link>
+            <Link
+              className="duration-500 hover:text-2xl"
+              href="#experience"
+              onClick={handleScroll}
+            >
+              Experience
+            </Link>
+            <Link
+              className="duration-500 hover:text-2xl"
+              href="#projects"
+              onClick={handleScroll}
+            >
+              Projects
+            </Link>
+          </div>
+        </div>
+
+        {/* Scrollable Content */}
+        <div className="w-1/2 px-32 ml-auto pb-96">
+          <div className="space-y-8" ref={aboutMeRef} id="about-me">
+            <h1 className="text-4xl">About Me</h1>
+            <p>
+              Lorem ipsum dolor, sit amet consectetur adipisicing elit. Fuga
+              eveniet mollitia praesentium repudiandae dolores quae harum atque
+              veritatis pariatur voluptatibus autem, nesciunt, enim nemo
+              laudantium voluptatum, illum et totam magni?
+            </p>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil
+              ipsa nobis sed hic magni quos, doloremque unde quia qui? Omnis
+              cupiditate sapiente corrupti necessitatibus ea perferendis, earum
+              quas alias consectetur?
+            </p>
+            <p>
+              Lorem ipsum dolor, sit amet consectetur adipisicing elit. Fuga
+              eveniet mollitia praesentium repudiandae dolores quae harum atque
+              veritatis pariatur voluptatibus autem, nesciunt, enim nemo
+              laudantium voluptatum, illum et totam magni?
+            </p>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil
+              ipsa nobis sed hic magni quos, doloremque unde quia qui? Omnis
+              cupiditate sapiente corrupti necessitatibus ea perferendis, earum
+              quas alias consectetur?
+            </p>
+            <p>
+              Lorem ipsum dolor, sit amet consectetur adipisicing elit. Fuga
+              eveniet mollitia praesentium repudiandae dolores quae harum atque
+              veritatis pariatur voluptatibus autem, nesciunt, enim nemo
+              laudantium voluptatum, illum et totam magni?
+            </p>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil
+              ipsa nobis sed hic magni quos, doloremque unde quia qui? Omnis
+              cupiditate sapiente corrupti necessitatibus ea perferendis, earum
+              quas alias consectetur?
+            </p>
+          </div>
+          <div className="pt-16" ref={experienceRef} id="experience">
+            <h1 className="text-4xl">Experience</h1>
+            <p>
+              Lorem ipsum dolor, sit amet consectetur adipisicing elit. Fuga
+              eveniet mollitia praesentium repudiandae dolores quae harum atque
+              veritatis pariatur voluptatibus autem, nesciunt, enim nemo
+              laudantium voluptatum, illum et totam magni?
+            </p>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil
+              ipsa nobis sed hic magni quos, doloremque unde quia qui? Omnis
+              cupiditate sapiente corrupti necessitatibus ea perferendis, earum
+              quas alias consectetur?
+            </p>
+            <p>
+              Lorem ipsum dolor, sit amet consectetur adipisicing elit. Fuga
+              eveniet mollitia praesentium repudiandae dolores quae harum atque
+              veritatis pariatur voluptatibus autem, nesciunt, enim nemo
+              laudantium voluptatum, illum et totam magni?
+            </p>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil
+              ipsa nobis sed hic magni quos, doloremque unde quia qui? Omnis
+              cupiditate sapiente corrupti necessitatibus ea perferendis, earum
+              quas alias consectetur?
+            </p>
+            <p>
+              Lorem ipsum dolor, sit amet consectetur adipisicing elit. Fuga
+              eveniet mollitia praesentium repudiandae dolores quae harum atque
+              veritatis pariatur voluptatibus autem, nesciunt, enim nemo
+              laudantium voluptatum, illum et totam magni?
+            </p>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil
+              ipsa nobis sed hic magni quos, doloremque unde quia qui? Omnis
+              cupiditate sapiente corrupti necessitatibus ea perferendis, earum
+              quas alias consectetur?
+            </p>
+          </div>
+          <div className="pt-16" ref={projectsRef} id="projects">
+            <h1 className="text-4xl">Projects</h1>
+            <p>
+              Lorem ipsum dolor, sit amet consectetur adipisicing elit. Fuga
+              eveniet mollitia praesentium repudiandae dolores quae harum atque
+              veritatis pariatur voluptatibus autem, nesciunt, enim nemo
+              laudantium voluptatum, illum et totam magni?
+            </p>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil
+              ipsa nobis sed hic magni quos, doloremque unde quia qui? Omnis
+              cupiditate sapiente corrupti necessitatibus ea perferendis, earum
+              quas alias consectetur?
+            </p>
+            <p>
+              Lorem ipsum dolor, sit amet consectetur adipisicing elit. Fuga
+              eveniet mollitia praesentium repudiandae dolores quae harum atque
+              veritatis pariatur voluptatibus autem, nesciunt, enim nemo
+              laudantium voluptatum, illum et totam magni?
+            </p>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil
+              ipsa nobis sed hic magni quos, doloremque unde quia qui? Omnis
+              cupiditate sapiente corrupti necessitatibus ea perferendis, earum
+              quas alias consectetur?
+            </p>
+            <p>
+              Lorem ipsum dolor, sit amet consectetur adipisicing elit. Fuga
+              eveniet mollitia praesentium repudiandae dolores quae harum atque
+              veritatis pariatur voluptatibus autem, nesciunt, enim nemo
+              laudantium voluptatum, illum et totam magni?
+            </p>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil
+              ipsa nobis sed hic magni quos, doloremque unde quia qui? Omnis
+              cupiditate sapiente corrupti necessitatibus ea perferendis, earum
+              quas alias consectetur?
+            </p>
+          </div>
         </div>
       </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
     </div>
   );
 }
